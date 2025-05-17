b8295351a578a717ec9f663222595ae6b4a243c8
Tryd0g0lik <work80@mail.ru>
2025-05-17 11:22:20 +0700

    [dev]:  update opions

--- src/sripts/iindex.ts
+++ src/sripts/iindex.ts
@@ -2,7 +2,7 @@ const handlerForm = async (event: MouseEvent): Promise<boolean>   => {
   event.stopPropagation();
   
   // GET SUBMIT HTML
-  if (event.type.toLowerCase() !== "submit") {
+  if (event.target && (event.target as HTMLElement).tagName.toLowerCase() !== "button") {
     return false;
   }
   event.preventDefault();
@@ -11,17 +11,27 @@ const handlerForm = async (event: MouseEvent): Promise<boolean>   => {
   if (currenttarget && (currenttarget as HTMLFormElement).tagName.toLowerCase() !== "form") {
     return false;
   }
+  const dataF0rm = new FormData(currenttarget as HTMLFormElement);
+  type DataForm = { [key: string]: string };
+  const dataObj = new FormData();
+  dataObj.append('title', dataF0rm.get('title')?.toString() as string);
+  dataObj.append("description", dataF0rm.get('description')?.toString() as string);
+  dataObj.append("category", dataF0rm.get('category')?.toString() as string);
+  dataObj.append("files", dataF0rm.get('files')?.toString() as string);
+  dataObj.append("path", dataF0rm.get('path')?.toString() as string);
+
   try{
-    const response = await fetch("api/v1/ads/",
+    const response = await fetch("http://127.0.0.1:8000/api/v1/ads/",
       {
         method: "POST",
-        body: new FormData(currenttarget as HTMLFormElement),
+        body: dataF0rm
       }
     );
     if (!response.ok){
       return false;
     }
-    // const body = await response.json();
+    const body = await response.json();
+    console.log(`SERVER: ${body}`);
     
   } catch (error : ErrorEvent| unknown){
     console.log(`Request to server Error => ${error}`);
@@ -31,13 +41,13 @@ const handlerForm = async (event: MouseEvent): Promise<boolean>   => {
 };
 
 function formPage(): boolean{
-  const formHTMLArr = document.getElementsByClassName("form") as HTMLCollectionOf<HTMLFormElement>;
-  if (formHTMLArr.length === 0){
+  const formHTML = document.querySelector(".form form") as HTMLFormElement;
+  if (!formHTML) {
     console.log("Somewing that frong! Invalid form.")
     return false;
   }
-  const formHTML = (formHTMLArr as HTMLCollectionOf<HTMLFormElement>)[0] ;
-  formHTML.onclick = handlerForm
+  const formHTMLCopy = (formHTML as HTMLFormElement);
+  formHTMLCopy.onclick = handlerForm
   return true;
 }
 // LISTENER CLICK BY FORM's SUBMIT BUTTOM
