e7367ae3b510528d445e86b517b81562e7c8af33
Tryd0g0lik <work80@mail.ru>
2025-06-10 07:35:25 +0700

    [dev]: Function was rename from 'src\adboard\app.ts::handlerCommmon' to the 'src\adboard\app.ts::handlerLoginLogout

--- src/adboard/app.ts
+++ src/adboard/app.ts
@@ -4,7 +4,7 @@
 import { asyncGetListenerEvent } from "src/scripts/handleRequsetReceiveAd";
 import { handlerUserForm } from "@ADBoards-handlers/handlerFormUsers/handlerRegisterForm";
 import { taskLogoutUser } from "@ADBoards/scripts/services/taskLogoutUser";
-export function handlerCommmon(): void {
+export function handlerLoginLogout(): void {
   if (window.location.pathname.includes("login") ||
   // LOGIN INTERFECE ADDING TO THE NAVBAR.
     window.location.pathname.includes('register')) {
@@ -15,8 +15,8 @@ export function handlerCommmon(): void {
   asyncGetListenerEvent("click", taskLogoutUser, undefined, "logout");
 };
 
-document.removeEventListener("DOMContentLoaded", () => handlerCommmon);
+document.removeEventListener("DOMContentLoaded", () => handlerLoginLogout);
 document.addEventListener("DOMContentLoaded", () => {
   console.log("START APP");
-  handlerCommmon();
+  handlerLoginLogout();
 });
