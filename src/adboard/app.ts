dd423150a52ebe1b278842fa43d29f0b72c915eb
Tryd0g0lik <work80@mail.ru>
2025-06-10 07:40:01 +0700

    [dev]: Relocation from  'src\adboard\app.ts::handlerLoginLogout' to the 'src\adboard\scripts\handlers\loginLogout.ts::handlerLoginLogout'

--- src/adboard/app.ts
+++ src/adboard/app.ts
@@ -1,19 +1,7 @@
 	/**
  * src\adboard\scripts\services\taskGetErrorContent.ts
  */
-import { asyncGetListenerEvent } from "src/scripts/handleRequsetReceiveAd";
-import { handlerUserForm } from "@ADBoards-handlers/handlerFormUsers/handlerRegisterForm";
-import { taskLogoutUser } from "@ADBoards/scripts/services/taskLogoutUser";
-export function handlerLoginLogout(): void {
-  if (window.location.pathname.includes("login") ||
-  // LOGIN INTERFECE ADDING TO THE NAVBAR.
-    window.location.pathname.includes('register')) {
-    console.log("START REGISTER");
-    asyncGetListenerEvent("keydown", handlerUserForm, undefined, "form-login");
-  };
-  // LOGOUT INTERFECE ADDEING TO THE NAVBAR.
-  asyncGetListenerEvent("click", taskLogoutUser, undefined, "logout");
-};
+import { handlerLoginLogout } from "@ADBoards-handlers/loginLogout";
 
 document.removeEventListener("DOMContentLoaded", () => handlerLoginLogout);
 document.addEventListener("DOMContentLoaded", () => {
---
+++ src/adboard/scripts/handlers/loginLogout.ts
@@ -0,0 +1,16 @@
+/**
+ * src\adboard\scripts\handlers\loginLogout.ts
+ */
+import { asyncGetListenerEvent } from "src/scripts/handleRequsetReceiveAd";
+import { handlerUserForm } from "@ADBoards-handlers/handlerFormUsers/handlerRegisterForm";
+import { taskLogoutUser } from "@ADBoards/scripts/services/taskLogoutUser";
+export function handlerLoginLogout(): void {
+  if (window.location.pathname.includes("login") ||
+    // LOGIN INTERFECE ADDING TO THE NAVBAR.
+    window.location.pathname.includes('register')) {
+    console.log("START REGISTER");
+    asyncGetListenerEvent("keydown", handlerUserForm, undefined, "form-login");
+  };
+  // LOGOUT INTERFECE ADDEING TO THE NAVBAR.
+  asyncGetListenerEvent("click", taskLogoutUser, undefined, "logout");
+};
